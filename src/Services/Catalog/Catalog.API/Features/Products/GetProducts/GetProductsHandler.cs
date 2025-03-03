c897dab6dbbd44e25b0336519411a768a7de3b20
pasternak2048 <10pka.muscatel@gmail.com>
2025-03-03 18:23:56 +0200

    Implemented Logging Behavior using IPipelineBehavior

--- src/Services/Catalog/Catalog.API/Features/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Features/Products/GetProducts/GetProductsHandler.cs
@@ -6,13 +6,11 @@ namespace Catalog.API.Features.Products.GetProducts
 
 	public record GetProductsResult(IEnumerable<Product> Products);
 
-	internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) :
+	internal class GetProductsQueryHandler(IDocumentSession session) :
 		IQueryHandler<GetProductsQuery, GetProductsResult>
 	{
 		public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
 		{
-			logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
-
 			var products = await session.Query<Product>().ToListAsync(cancellationToken);
 
 			return new GetProductsResult(products);
