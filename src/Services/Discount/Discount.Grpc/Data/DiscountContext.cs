a5eee8aed23b3a206fe37d74e6118fd7aee98aa3
pasternak2048 <10pka.muscatel@gmail.com>
2025-03-10 20:53:02 +0200

    Added mysql for Discount service

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,24 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+	public class DiscountContext : DbContext
+	{
+		public DiscountContext(DbContextOptions<DiscountContext> options) 
+			: base(options)
+		{
+		}
+
+		public DbSet<Coupon> Coupons { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Coupon>().HasData(
+				new Coupon { Id = 1, ProductName = "Gaming PC - RTX 4080", Description = "High-performance gaming PC with RTX 4080 graphics card and 32GB RAM.", Amount = 150},
+				new Coupon { Id = 2, ProductName = "Wireless Gaming Mouse", Description = "Ergonomic wireless mouse with customizable RGB lighting.", Amount = 200 }
+				);
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
