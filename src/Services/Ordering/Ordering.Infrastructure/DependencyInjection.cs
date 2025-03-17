477a83c965af081a2664bd300e0f9277c89a33a3
pasternak2048 <10pka.muscatel@gmail.com>
2025-03-17 18:43:15 +0200

    Implemented DispatchDomainEventsInterceptor

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Infrastructure
@@ -9,14 +10,16 @@ namespace Ordering.Infrastructure
         {
             var connectionString = configuration.GetConnectionString("Database");
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-            {
-				options.AddInterceptors(new AuditableEntityInterceptor());
+			services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+			services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
 
+			services.AddDbContext<ApplicationDbContext>((sp, options) =>
+			{
+				options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
 				options.UseSqlServer(connectionString);
-            });
+			});
 
-            return services;
+			return services;
 		}
     }
 }
